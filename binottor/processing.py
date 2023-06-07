784702115a42bda7ccfd07902aea09a732b41ad9
Etienne Ginoux <ginoux.etienne@gmail.com>
2023-06-07 14:59:26 +0200

    Modified params for tyre stress function

--- binottor/processing.py
+++ binottor/processing.py
@@ -16,51 +16,8 @@ TIRE_MATCH = {
     'UNKNOWN': 'UNKNOWN'
 }
 
-def load_dataset():
-    laps_df = pd.read_csv(os.path.join(abs,"../raw_data/laps.csv"))
-    weather_df = pd.read_csv(os.path.join(abs,"../raw_data/weather.csv"))
-    track_status_df = pd.read_csv(os.path.join(abs,"../raw_data/track_status.csv"))
-    return laps_df, weather_df, track_status_df
-
-
-def compound_recategorization(laps,tire_mapping):
-    new_laps = laps.copy()
-    new_laps['Compound']=laps['Compound'].map(tire_mapping)
-    return new_laps
-
-def compound_cleaning(laps,tire_mapping,backfilling=3):
-    new_laps = laps.copy()
-    new_laps = compound_recategorization(new_laps,tire_mapping)
-    new_laps['Compound'].replace('UNKNOWN',None,inplace=True)
-    new_laps['Compound'].fillna(method="bfill",limit=backfilling,inplace=True)
-    return new_laps
-
-def tire_degradation_offset(laps):
-    pass
-
-def check_second_compound():
-    pass
-  
-def add_race_progress(df):
-    # Group data to get lap number per year per race
-    grouped_data = df.groupby(by = ["Year", "Location"], as_index=False)["LapNumber"].max().rename(columns={"LapNumber":"TotalLaps"})
-    grouped_data["Year_Location"] = grouped_data["Year"].map(str) + grouped_data["Location"]
-    # Group data to get same info as grouped_data
-    df["Year_Location"] = df["Year"].map(str) + df["Location"]
-    # Merge data
-    df = df.merge(grouped_data, on="Year_Location")
-    # Clean data frame
-    df.drop(columns=["Year_Location", "Year_y", "Location_y"], inplace=True)
-    df["RaceProgress"] = df["LapNumber"] / df["TotalLaps"]
-    df.rename(columns={"Location_x": "Location", "Year_x": "Year"}, inplace=True)
-    return df
-
-def mask_race_percentage(df, percentage):
-    df = df[df["RaceProgress"] > percentage]
-    return df
-
-def get_tyre_stress_level(df):
-    params = {'Mexico City': 4,
+TYRE_STRESS = {
+    'Mexico City': 4,
     'Lusail': 1,
     'Shanghai': 2,
     'Mugello': 1,
@@ -94,6 +51,52 @@ def get_tyre_stress_level(df):
     'Yas Island': 3,
     'Le Castellet': 2,
     'Sakhir': 3,
-    'Silverstone': 1}
-    df["TyreStressLevel"] = df["Location"].map(params)
+    'Silverstone': 1
+}
+
+def load_dataset():
+    laps_df = pd.read_csv(os.path.join(abs,"../raw_data/laps.csv"))
+    weather_df = pd.read_csv(os.path.join(abs,"../raw_data/weather.csv"))
+    track_status_df = pd.read_csv(os.path.join(abs,"../raw_data/track_status.csv"))
+    return laps_df, weather_df, track_status_df
+
+
+def compound_recategorization(laps,tire_mapping):
+    new_laps = laps.copy()
+    new_laps['Compound']=laps['Compound'].map(tire_mapping)
+    return new_laps
+
+def compound_cleaning(laps,tire_mapping,backfilling=3):
+    new_laps = laps.copy()
+    new_laps = compound_recategorization(new_laps,tire_mapping)
+    new_laps['Compound'].replace('UNKNOWN',None,inplace=True)
+    new_laps['Compound'].fillna(method="bfill",limit=backfilling,inplace=True)
+    return new_laps
+
+def tire_degradation_offset(laps):
+    pass
+
+def check_second_compound():
+    pass
+
+def add_race_progress(df):
+    # Group data to get lap number per year per race
+    grouped_data = df.groupby(by = ["Year", "Location"], as_index=False)["LapNumber"].max().rename(columns={"LapNumber":"TotalLaps"})
+    grouped_data["Year_Location"] = grouped_data["Year"].map(str) + grouped_data["Location"]
+    # Group data to get same info as grouped_data
+    df["Year_Location"] = df["Year"].map(str) + df["Location"]
+    # Merge data
+    df = df.merge(grouped_data, on="Year_Location")
+    # Clean data frame
+    df.drop(columns=["Year_Location", "Year_y", "Location_y"], inplace=True)
+    df["RaceProgress"] = df["LapNumber"] / df["TotalLaps"]
+    df.rename(columns={"Location_x": "Location", "Year_x": "Year"}, inplace=True)
+    return df
+
+def mask_race_percentage(df, percentage):
+    df = df[df["RaceProgress"] > percentage]
+    return df
+
+def get_tyre_stress_level(df):
+    df["TyreStressLevel"] = df["Location"].map(TYRE_STRESS)
     return df
