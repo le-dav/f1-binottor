6f3b873dad8459aded38086f128af711a7e011a2
Etienne Ginoux <ginoux.etienne@gmail.com>
2023-06-08 14:45:07 +0200

    MAJ des fonctions

---
+++ binottor/main.py
@@ -0,0 +1,8 @@
+from processing import *
+
+# def preproc(df):
+#     df = fill_team_name(df)
+#     df = fill_driver(df)
+#     df = fill_personal_best(df)
+#     df = fill_position(df)
+#     return df
