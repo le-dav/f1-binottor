62d03d5fda10f95da866ac0ea663c7336cb375ce
Margaux Martinez <margauxmartinez2801@orange.fr>
2023-06-13 16:04:20 +0200

    modifying fast.py

---
+++ binottor/model.py
@@ -0,0 +1,21 @@
+import pickle
+
+def initialize_model():
+    pass
+
+def compile_model():
+    pass
+
+def train_model():
+    pass
+
+def evaluate_model():
+    pass
+
+
+
+def load_model_compound() :
+    return pickle.load(open("model_compound.pkl","rb"))
+
+def load_model_pit():
+    return pickle.load(open("model_pit.pkl","rb"))
